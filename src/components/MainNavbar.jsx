192c4e215baaf9942e5759d6dafa41e20a9e868e
ricardo <ohcanikissu@gmail.com>
2023-12-12 20:15:46 +0800

    style:主体样式调整

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -4,8 +4,8 @@ import {mainNavbarMid, mainNavbarRight} from "../contants/data.js";
 
 const MainNavbar = () => {
     return <div className={`flex items-center my-0 mx-auto 
-    md:mx-[20px] max-w-[2560px] w-full h-[100px] px-[140px]
-    md:px-[24px]
+    md:mx-[20px] max-w-[2560px] w-full h-[140px] md:h-[100px] px-[140px]
+    md:px-[24px] md:w-[1180px]
     `}>
         {/*left*/}
         <div className={'flex items-center'}>
@@ -24,13 +24,13 @@ const MainNavbar = () => {
         {/*right*/}
         <div className={'flex items-center justify-between w-full pr-[30px]'}>
             {/* mid */}
-            <div className={`grid grid-rows-[repeat(2,1fr)] md:grid-cols-[repeat(9,1fr)] 
+            <div className={`grid grid-rows-[repeat(2,1fr)] md:grid-cols-[repeat(9,1fr)]
             grid-flow-col gap-[10px] w-full pr-[20px] border-solid border-r-[1px] border-[#e3e5e7]
             `}>
                 {mainNavbarMid.map((item, index) => (
-                    <a href={'#'} className={`h-[26px] leading-[26px]
-                        inline-block w-full rounded-[6px] border-[1px]
-                        border-[#f1f2f3] tracking-[2px] box-content 
+                    <a href={'#'} className={`md:h-[26px] md:leading-[26px]
+                        h-[30px] leading-[30px] inline-block w-full rounded-[6px] border-[1px]
+                        border-[#f1f2f3] tracking-[2px] box-content ${index > 17 && 'md:hidden'}
                         bg-[#f6f7f8] text-center font-normal text-[16px] md:text-[13px] text-[#61666d]`}>
                         {item}
                     </a>
@@ -39,7 +39,7 @@ const MainNavbar = () => {
             {/* right */}
             <div className={'grid grid-rows-[repeat(2,1fr)] gap-y-[10px] grid-flow-col w-[240px] shrink-0'}>
                 {mainNavbarRight.map((item, index) => (
-                    <a href={'#'} className={'flex items-center justify-end text-[16px] font-normal md:text-[13px] text-[#61666d]'}>
+                    <a href={'#'} className={`flex items-center justify-end text-[16px]  font-normal md:text-[13px] text-[#61666d]`}>
                         <DynamicHeroIcon icon={item.icon}/>
                         <span>{item.title}</span>
                     </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ export default {
   theme: {
     extend: {},
     screens: {
-      'md': {'max': '1223px'},
+      'md': {'max': '1403px'},
       // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      'lg': {'min': '1234px', 'max': '1700px'},
+      'lg': {'min': '1404px', 'max': '1979px'},
       // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      '2xl': {'min': '1701px'},
+      '2xl': {'min': '1980px'},
     },
   },
   plugins: [],
